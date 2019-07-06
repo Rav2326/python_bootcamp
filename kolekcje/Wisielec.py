a18e1bd3cbbc924c2de9ee519563ef7d16aba514
Rav2326 <51874379+Rav2326@users.noreply.github.com>
2019-07-06 10:01:13 +0200

    Add files via upload

---
+++ kolekcje/Wisielec.py
@@ -0,0 +1,24 @@
+imie = str(input("Jak masz na imie ?: "))
+haslo = "python"
+szanse = 0
+wynik = ["_" for x in haslo]
+DEBUG = True
+
+while True:
+    litera = input("Podaj litere: ")
+
+
+    if litera in haslo:
+        for litera2 in haslo:
+            if litera == litera2:
+                wynik.append("_")
+    else:
+        wynik.append("_")
+
+
+
+# wynik = ["_" for x in slowo]
+#jest to samo co:
+# wynik = [}
+# for x i9n slowo:
+    #wynik.append("_")
