53eadb3e50ca095d84ee212d461f983235533f34
Rav2326 <51874379+Rav2326@users.noreply.github.com>
2019-07-06 12:16:01 +0200

    Add files via upload

---
+++ kolekcje/Zadanie_10.py
@@ -0,0 +1,20 @@
+napis= input("Podaj napis: ")
+
+#napis = "Ala ma konta"
+liczniki = {}
+# for l in napis:
+#      if l in liczniki:
+#          liczniki[l] += 1
+#      else:
+#          liczniki[1] = 1
+#
+# print(liczniki)
+
+# for l in napis:
+#     liczniki[l] = liczniki.get(1, 0) + 1
+
+for l in napis:
+    liczniki[l] = napis.count(l)
+
+print(liczniki)
+
---
+++ kolekcje/wyrania_listowe.py
@@ -0,0 +1,2 @@
+print([x**2 for x in range(100) if x % 2 == 0 ])
+print({x**3 for x in range(100) if x % 2 == 0 })
