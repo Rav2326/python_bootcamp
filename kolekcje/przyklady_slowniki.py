46d910db3b145d19b8e43d684a72eaa0318af697
Rav2326 <51874379+Rav2326@users.noreply.github.com>
2019-07-06 11:12:40 +0200

    Add files via upload

---
+++ kolekcje/przyklady_slowniki.py
@@ -0,0 +1,24 @@
+osoba={
+
+    "imie": "Rafał",
+    "nazwisko": "Handzlik",
+    "hobby": ["muzyka", "historia", "..."],
+    "dzieci": {"Corka1", "Garbyiela" },
+    1: "jeden"
+}
+
+print(osoba["imie"])
+print(osoba["nazwisko"])
+print(osoba[1])
+
+# dzieci = osoba["dzieci"]
+# # dzieci["Corka1"] = "Gabi"
+# # print(osoba)
+
+osoba["sport"] = ["armwrestling"]
+print(osoba.get("zona", "brak")) #w przytpadku gdy nie ma "zona" daje sie komende .get
+print(osoba.keys())
+#print(osoba.pop)
+print(osoba.values())
+
+
