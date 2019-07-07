aa38ef74be294d3ee51137a9d784f667d1416171
Rafał <handzlikrafa@gmail.com>
2019-07-07 16:02:42 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Funkcje/Przykładu.py
    #	Funkcje/ZAdanie_1.py
    #	Funkcje/ZAdanie_2.py
    #	Funkcje/ZAdanie_3.py
    #	Funkcje/ZAdanie_4.py
    #	Funkcje/przyklady_argumenty_wartosci_domyslnej.py
    #	kolekcje/Sortowanie_przez_wybieranie.py
    #	kolekcje/Wisielec.py
    #	kolekcje/Zadanie_10.py
    #	kolekcje/Zadanie_11.py
    #	kolekcje/Zadanie_9.py
    #	kolekcje/przyklady_slowniki.py
    #	kolekcje/wisielecrozwiazanie.py
    #	kolekcje/wyrania_listowe.py

--- Funkcje/ZAdanie_7.py
+++ Funkcje/ZAdanie_7.py
@@ -1,8 +1,38 @@
+# przytnij(
+# data= [1, 2, 3, 4, 5, 6, 7],
+# start = lambda x: x >3,
+# stop = lambda x: x> 3,
+# )
+#
+# ["a", "b", "c", 2, 4, "dy", "x",  5, 6, "a", "b", "c", "dy", "x"]
+#
+# przytnij(
+# data= ["a", "b", "c", "dy", "x"],
+# start = lambda x: x == "b",
+# stop = lambda x: ,
+# )
+
+def przytnij(data, start, stop):
+    rezultat = []
+    czy_dodawac = False
+
+    for element in data:
+        if start(element):
+            czy_dodawac = True
+
+        if czy_dodawac:
+            rezultat.append(element)
+            if stop(element):
+                break
+                # czy_dodawac = False
+    return rezultat
 
 
-data= [1, 2, 3, 4, 5, 6, 7],
-start = lambda x: x >3,
-stop = lambda x: x> 3,
+def test_przytnij():
+    actual = przytnij(
+        data=[1, 2, 3, 4, 1, 6, 7],
+        start=lambda x: x > 3,
+        stop=lambda x: x == 6
+    )
 
-print(start)
-print(stop)
+    assert actual == [4, 1, 6]
